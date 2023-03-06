394929f071c50105f87af0bd2a5e02b652879889
Hyunjin Park <meat124@naver.com>
2023-03-06 19:47:27 +0900

    2023_03_06 Codeforces_Round #855 #A 정답코드 보고 최적화

--- Codeforces_Round#855(Div.3)/#A_Is_It_a_Cat.py
+++ Codeforces_Round#855(Div.3)/#A_Is_It_a_Cat.py
@@ -1,26 +1,12 @@
-from collections import Counter
+from collections import OrderedDict
 import sys
 input = sys.stdin.readline
-result = {'M': 'MmEe', 'm': 'MmEe', 'E': 'EeOo', 'e': 'EeOo',
-          'O': 'OoWw', 'o': 'OoWw', 'W': 'Ww', 'w': 'Ww'}
 
 for t in range(int(input())):
     n = int(input())
-    s = input().rstrip()
-    if n < 4:
-        print('NO')
-        continue
-    s_keys = Counter(s).keys()
-    r = 'YES'
-    for i in range(n - 1):
-        if s[i] not in 'MmEeOoWw':
-            r = 'NO'
-            break
-        if s[i + 1] in result[s[i]]:
-            continue
-        else:
-            r = 'NO'
-            break
-    if not (s[0] in 'Mm' and s[-1] in 'Ww'):
-        r = 'NO'
-    print(r)
+    s = input().rstrip().lower()
+    S = []
+    for i in s:
+        if not S or i != S[-1]:
+            S.append(i)
+    print('YES') if ''.join(S) == 'meow' else print('NO')
